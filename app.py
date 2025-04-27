9be1ccc45c6fdfaf5fb9269202f3bff1ef9120d7
Ashe <katieaspivey@gmail.com>
2025-04-27 17:21:47 -0400

    fix error where data was not properly being saved

--- app.py
+++ app.py
@@ -2,30 +2,40 @@ import typer
 
 app = typer.Typer()
 # for whatever reason the tasks list is not being saved to and read from T-T
-tasks = []
 
 # this comment made from WSL
 # testing to ensure that pushing works
 
 @app.command()
-def menu():
-    task_file = open("data.txt", "a")
-    print(f"hello! here are your current tasks.")
-    taskCount = 0
-    for task in tasks:
-        task_file.write(f"({taskCount}.) - {task}\n")
-        print(f"{task}")
-        taskCount += 1
-    task_file.close()
-    task_file_read = open("data.txt", "r")
-    print(task_file_read.read())
-    
+def load():
+    with open("data.txt", "r") as save_data:
+        for line in save_data.readlines():
+            tasks.append(line)
 
 @app.command()
-def addtask(desc: str):
-    tasks.append(desc)
-    print(f"task added: {desc}")
+def menu():
+    task_file = open("data.txt", "r")
+    print(f"hello! here are your current tasks.")
+    #for task in task_file.readline():
+    #    print(f"{task}")
+    #task_file.close()
+
+    with open ("data.txt", "r") as data:
+        for line in data:
+            print(line)
+    
+#@app.command()
+#def addtask(desc: str):
+    
+#    print(f"task added: {desc}")
+
+@app.command()
+def write(task: str):
+    task_file = open ("data.txt", "a")
+    task_file.write(f"- {task}")
+    task_file.close()
 
 if __name__ == "__main__":
+    global tasks
     app()
     
