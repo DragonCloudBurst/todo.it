b93349cae4b29fa0d61cd6946d1fdd997780174f
Ashe <katieaspivey@gmail.com>
2025-04-27 17:26:43 -0400

    create function to wipe save data

--- app.py
+++ app.py
@@ -13,21 +13,23 @@ def load():
             tasks.append(line)
 
 @app.command()
+def wipe():
+    print("Proceeding with this action will delete all of your save data.")
+    choice = input("Are you sure you want to proceed? (y/n)")
+    if choice == "y" or choice == "Y":
+        with open("data.txt", "w") as save_data:
+            save_data.write("")
+    else:
+        print("Command cancelled.")
+
+@app.command()
 def menu():
     task_file = open("data.txt", "r")
     print(f"hello! here are your current tasks.")
-    #for task in task_file.readline():
-    #    print(f"{task}")
-    #task_file.close()
 
     with open ("data.txt", "r") as data:
         for line in data:
             print(line)
-    
-#@app.command()
-#def addtask(desc: str):
-    
-#    print(f"task added: {desc}")
 
 @app.command()
 def write(task: str):
